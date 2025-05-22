10875bcb62161208210b674672b1db1b84a007e6
VanamaVarunKumar <131695296+VanamaVarunKumar@users.noreply.github.com>
2025-05-22 22:11:05 +0530

    Strings

---
+++ tns/StrReverse.java
@@ -0,0 +1,18 @@
+package com.tns.strings;
+public class StrReverse {
+public static void main(String[] args) {
+//String str="mam";
+//String rev =new StringBuilder(str).reverse().toString();
+//if (str.equals(rev)){
+//	System.out.println("Equal");
+//}
+//else {
+//	System.out.println("Not Equal");
+//}
+//System.out.println(rev);
+//	}
+//}
+String str="varun";
+System.out.println(str.toUpperCase());
+}
+}
